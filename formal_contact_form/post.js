f42b2244ffc9eb8b41e8f2c932f1c46312f04408
Aniket Munshi <Aniket Munshi>
2016-12-26 12:46:17 +0530

    Added view all functionality with express-handlebars

--- formal_contact_form/post.js
+++ formal_contact_form/post.js
@@ -2,8 +2,10 @@ var exp = require('express');
 var app = exp();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Contact = require('./model');
-
+var Contact = require('./model'); 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
+app.set('view engine', 'handlebars');
 
 mongoose.connect('mongodb://localhost/example');
 
@@ -19,16 +21,19 @@ app.get('/index.html', function(req, res){
 
 app.get('/contacts', function(req, res) {
   console.log('getting all contacts');
-  Contact.find({})
+  var temp = Contact.find({})
     .exec(function(err, contacts) {
       if(err) {
         res.send('error occured')
       } else {
         console.log(contacts);
-        res.json(contacts);
+        res.render('y', {temp: contacts} );
+        
       }
     });
 });
+
+
 
 app.get('/contacts/:id', function(req, res) {
   console.log('getting all contacts');
@@ -46,21 +51,27 @@ app.get('/contacts/:id', function(req, res) {
 });
 //*/
 app.post('/data', function(req, res) {
+
   var newEntry = new Contact();
 
-  newEntry.name = req.body.name;
-  newEntry.email = req.body.email;
-  newEntry.mobile = req.body.mobile;
+  var x = newEntry.name = req.body.name;
+  var y = newEntry.email = req.body.email;
+  var z = newEntry.mobile = req.body.mobile;
 
   newEntry.save(function(err, data) {
     if(err) {
       res.send('error saving data');
     } else {
       console.log(data);
-      res.send(data);
     }
-  });
+  var data = {
+    name: x, email: y, mobile: z
+  };
+  res.render('x', data);
 });
+
+})
+
 /*
 app.post('/data2', function(req, res) {
   Book.create(req.body, function(err, book) {
@@ -70,32 +81,35 @@ app.post('/data2', function(req, res) {
       console.log(book);
       res.send(book);
     }
-  });
+  }); 
 });
 //*/
 
-app.put('/contact/:id', function(req, res){
+app.put('/up', function(req, res){
 	Contact.findOneAndUpdate({name: req.params.name}, {$set:{name:req.body.n_name, email: req.body.email, mobile: req.body.mobile}}, {upsert:false}, function(err, data){
 			if(err){
-				console.log('Error Encountered!')
+				console.log('Error Encountered!');
 			}else{
 				console.log(data);
+        res.render('w')
 				res.send(204);
 			}
 	})
 }) 
 
-app.delete('/contact/:id', function(req, res) {
-  Contact.findOneAndRemove({name: req.params.name}, function(err, data) {
+app.delete('/del', function(req, res) {
+  var x = req.query.name;
+  console.log(x);
+  Contact.findOneAndRemove({x}, function(err, data) {
     if(err) {
       res.send('error removing')
     } else {
-      console.log(data);
-      res.status(204);
+      res.render('z', {name:x});
     }
   });
 });
 //*/
+
 app.listen(8081, function() {
   console.log('Server running on http://localhost:' + 8081);
 });
